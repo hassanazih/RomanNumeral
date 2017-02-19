1256a82f69eb823151f8005b3a6039a7e5b14f06
hassan <hassanazih@gmail.com>
2017-02-19 16:22:39 +0100

    make second tests suite pass

--- src/main/java/fr/sg/DecimalToRomanNumeralConverter.java
+++ src/main/java/fr/sg/DecimalToRomanNumeralConverter.java
@@ -5,8 +5,16 @@ public class DecimalToRomanNumeralConverter
 
     public static String convert(int decimal)
     {
-        if(decimal == 1)
-            return "I";
-        return "X";
+        switch(decimal)
+        {
+            case 1: return "I";
+            case 5: return "V";
+            case 10: return "X";
+            case 50: return "L";
+            case 100: return "C";
+            case 500: return "D";
+            case 1000:return "M";
+        }
+        return "Undefined";
     }
 }
