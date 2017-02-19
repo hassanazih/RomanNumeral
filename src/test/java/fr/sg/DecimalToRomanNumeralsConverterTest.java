e5045de6f0944c5c9529dbbc2b3578cb9fd4eeb8
hassan <hassanazih@gmail.com>
2017-02-19 16:20:01 +0100

    add another test suite

--- src/test/java/fr/sg/DecimalToRomanNumeralsConverterTest.java
+++ src/test/java/fr/sg/DecimalToRomanNumeralsConverterTest.java
@@ -11,4 +11,39 @@ public class DecimalToRomanNumeralsConverterTest
     {
         Assertions.assertThat(DecimalToRomanNumeralConverter.convert(1)).isEqualTo("I");
     }
+
+    @Test
+    public void should_return_V_when_input_is_5() throws Exception
+    {
+        Assertions.assertThat(DecimalToRomanNumeralConverter.convert(5)).isEqualTo("V");
+    }
+
+    @Test
+    public void should_return_X_when_input_is_10() throws Exception
+    {
+        Assertions.assertThat(DecimalToRomanNumeralConverter.convert(10)).isEqualTo("X");
+    }
+
+    @Test
+    public void should_return_L_when_input_is_50() throws Exception
+    {
+        Assertions.assertThat(DecimalToRomanNumeralConverter.convert(50)).isEqualTo("L");
+    }
+
+    @Test
+    public void should_return_C_when_input_is_100() throws Exception
+    {
+        Assertions.assertThat(DecimalToRomanNumeralConverter.convert(100)).isEqualTo("C");
+    }
+    @Test
+    public void should_return_D_when_input_is_500() throws Exception
+    {
+        Assertions.assertThat(DecimalToRomanNumeralConverter.convert(500)).isEqualTo("D");
+    }
+
+    @Test
+    public void should_return_M_when_input_is_1000() throws Exception
+    {
+        Assertions.assertThat(DecimalToRomanNumeralConverter.convert(1000)).isEqualTo("M");
+    }
 }
