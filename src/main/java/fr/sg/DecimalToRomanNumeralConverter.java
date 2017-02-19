771bff4550ebeae70920bd9048c5b6220b6a6c14
hassan <hassanazih@gmail.com>
2017-02-19 16:00:57 +0100

    Create kata's skeleton

---
+++ src/main/java/fr/sg/DecimalToRomanNumeralConverter.java
@@ -0,0 +1,9 @@
+package fr.sg;
+
+public class DecimalToRomanNumeralConverter
+{
+    public static String convert(int decimal)
+    {
+
+    }
+}
---
+++ src/test/java/fr/sg/DecimalToRomanNumeralsConverterTest.java
@@ -0,0 +1,6 @@
+package fr.sg;
+
+public class DecimalToRomanNumeralsConverterTest
+{
+
+}
