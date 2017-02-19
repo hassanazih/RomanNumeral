1dbd49d1bd2d814a05c01ab14ea083cf2c846503
hassan <hassanazih@gmail.com>
2017-02-19 16:13:02 +0100

    First test case : Red

--- src/test/java/fr/sg/DecimalToRomanNumeralsConverterTest.java
+++ src/test/java/fr/sg/DecimalToRomanNumeralsConverterTest.java
@@ -1,6 +1,14 @@
 package fr.sg;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
 public class DecimalToRomanNumeralsConverterTest
 {
 
+    @Test
+    public void should_return_I_when_input_is_1() throws Exception
+    {
+        Assertions.assertThat(DecimalToRomanNumeralConverter.convert(1)).isEqualTo("I");
+    }
 }
