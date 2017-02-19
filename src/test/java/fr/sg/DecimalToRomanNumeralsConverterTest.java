00a2f0db480cf3bb7c593db8bfa7500b65fc20ab
hassan <hassanazih@gmail.com>
2017-02-19 16:52:04 +0100

    finish the suite tests with a fine algorithm

--- src/test/java/fr/sg/DecimalToRomanNumeralsConverterTest.java
+++ src/test/java/fr/sg/DecimalToRomanNumeralsConverterTest.java
@@ -2,6 +2,7 @@ package fr.sg;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
@@ -22,7 +23,7 @@ public class DecimalToRomanNumeralsConverterTest {
     @Parameterized.Parameters
     public static Collection addedNumbers() {
         return Arrays.asList(new Object[][]{{1, "I"}, {5, "V"},
-                {10, "X"}, {50, "L"}, {100, "C"}, {500, "D"}, {1000, "M"}});
+                {10, "X"}, {50, "L"}, {100, "C"}, {500, "D"}, {1000, "M"}, {2, "II"}, {3, "III"}, {4, "IV"}, {9, "IX"}, {40, "XL"}, {90, "XC"}, {240, "CCXL"},{3999,"MMMCMXCIX"}});
     }
 
     @Test
